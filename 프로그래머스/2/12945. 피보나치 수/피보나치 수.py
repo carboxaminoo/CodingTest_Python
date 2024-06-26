e2e4be76bc10d55dc6342e718706d71d4ce0064a
HyeJi_Kim <carboxamino@daum.net>
2024-06-26 22:50:10 +0900

    [level 2] Title: 피보나치 수, Time: 91.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,4 +1,13 @@
+# 처음에 재귀로 풀었더니 런타임 에러 발생
+def F(n):
+    if n in [0,1]:
+        return n
+    else:
+        return F(n-1)+F(n-2)
+    
+    
 def solution(n):
+
     a, b = 0, 1
     for i in range(2, n + 1):
         a, b = b, a + b
